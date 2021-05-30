aff9be9bd269236f48a751b7610ab32c77adb879
ravi <rkant062@gmail.com>
2021-05-30 16:49:47 +0530

    Add support for profit collation

--- Stock Project/stock_er.py
+++ Stock Project/stock_er.py
@@ -16,6 +16,7 @@ class Logger(object):
         self.logFile.seek(0, 2)
 
     def log(self, stringToLog):
+        print(stringToLog+"\n\n")
         self.logFile.write(stringToLog)
 
    # def __del__(self):
@@ -23,23 +24,67 @@ class Logger(object):
 
 
 class Stocker(Logger):
+    portfolio_ = []
+    local_demographic_ = []
 
-    def __init__(self):
-        Logger.__init__(self, fileName= 'stocker_'+str(time.time()))
-        
+    def _initialize_portfolio(self, data):
+        self.portfolio_ = pd.DataFrame()
+        self.portfolio_ = pd.DataFrame({'Stock' : [data.loc[0, 'Stock']], 'Transaction Amount' : [data.loc[0, 'Cost Price']], 'Remaining Units' : [data.loc[0, 'Units']]})
+        #print(self.portfolio_)
+        for i in range(1, len(data)):
+            #print(data.loc[i, 'Stock'] not in self.portfolio_.values)
+            if(data.loc[i, 'Stock'] not in self.portfolio_.values):
+                self.log("{} New Stock added to portfolio, Creating new entry in hash".format(data.loc[i, 'Stock']))
+                self.portfolio_.loc[len(self.portfolio_.index)] = [data.loc[i, 'Stock'], data.loc[i, 'Cost Price'], data.loc[i, 'Units']]
+            else:
+                if(data.loc[i, 'Transaction']=='B'):
+                    self.portfolio_.loc[self.portfolio_['Stock'] == data.loc[i, 'Stock'], 'Transaction Amount'] += data.loc[i, 'Cost Price']
+                    self.portfolio_.loc[self.portfolio_['Stock'] == data.loc[i, 'Stock'], 'Remaining Units'] = self.portfolio_.loc[self.portfolio_['Stock'] == data.loc[i, 'Stock'], 'Transaction Amount'] / self._get_market_price(data.loc[i, 'Stock']) * 1.0;
+                    self.log("BUY: Total amount invested YTD in {} is {}".format(data.loc[i, 'Stock'], self.portfolio_.loc[self.portfolio_["Stock"]==data.loc[i, 'Stock'], "Transaction Amount"].iloc[0]))
+                else:
+                    self.portfolio_.loc[self.portfolio_['Stock'] == data.loc[i, 'Stock'], 'Transaction Amount'] -= data.loc[i, 'Cost Price']
+                    self.portfolio_.loc[self.portfolio_['Stock'] == data.loc[i, 'Stock'], 'Remaining Units'] = self.portfolio_.loc[self.portfolio_['Stock'] == data.loc[i, 'Stock'], 'Transaction Amount'] / self._get_market_price(data.loc[i, 'Stock']) * 1.0;
+                    self.log("SELL: Total amount invested YTD in {} is {}".format(
+                        data.loc[i, 'Stock'],
+                        self.portfolio_.loc[self.portfolio_["Stock"]==data.loc[i, 'Stock'], "Transaction Amount"].iloc[0]))
+
+    
     def _get_market_price(self, asset_name):
         temp = yf.Ticker(asset_name)
         self.log("{} : Reading stock {} at {} ".format(datetime.datetime.utcnow(), asset_name, threading.currentThread().getName))
         return temp.info['regularMarketPrice']
 
+    def _collate_buy_and_sell_transactions(self):
+        self.log("{} Collating sell and buy transactions".format(datetime.datetime.utcnow()));
+        self._initialize_portfolio(self.local_demographic_)
+
+    def reduce_profits(self):
+        sum = 0
+        for i in range(0, len(self.portfolio_)):
+            sum += self.portfolio_.loc[i, 'Transaction Amount']
+        if sum < 0:
+            self.log("may-day: Portfolio is in loss")
+        else:
+            self.log("woo-hoo, making money with Rs. {} in stocks".format(self.portfolio_["Transaction Amount"].sum()))
+
+
     def get_stock_demographics(self, file_input_df):
+        self.local_demographic_ = file_input_df
         for i in range(0, len(file_input_df['Stock']), 1):
-            file_input_df.loc[i,'CMP'] = self._get_market_price(file_input_df.loc[i,'Stock'])
-            file_input_df.loc[i, 'Profits/Loss ETD'] = file_input_df.loc[i,'CMP'] - file_input_df.loc[i,'Price']
-            file_input_df.loc[i, 'Percentage Change'] = str(file_input_df.loc[i, 'Profits/Loss ETD'] / file_input_df.loc[i,'Price'] * 100) + str(' %')
-        return file_input_df
+            self.local_demographic_.loc[i, 'CMP'] = self._get_market_price(self.local_demographic_.loc[i,'Stock'])
+            self.local_demographic_.loc[i, 'Cost Price'] = self.local_demographic_.loc[i,'Price'] * self.local_demographic_.loc[i,'Units']
+            self.local_demographic_.loc[i, 'Profits/Loss ETD'] = self.local_demographic_.loc[i,'CMP'] * self.local_demographic_.loc[i,'Units'] - self.local_demographic_.loc[i,'Cost Price']
+            self.local_demographic_.loc[i, 'Percentage Change'] = str(self.local_demographic_.loc[i, 'Profits/Loss ETD'] / self.local_demographic_.loc[i,'Cost Price'] * 100) + str(' %')
+        self._collate_buy_and_sell_transactions()
+        self.reduce_profits()
+        return self.local_demographic_
 
+    def __init__(self, data):
+        Logger.__init__(self, fileName= 'stocker_'+str(time.time()))
 
+    def __del__(self):
+        print(self.portfolio_)
+    
 
 class FileHandler(Logger):
 
@@ -51,16 +96,18 @@ class FileHandler(Logger):
         df = pd.read_csv(file_path)
         return df
 
-    def write_user_file(self, file_path):
-        print("Placeholder")
+    def write_user_file(self, data):
+        data.to_csv('derived_.csv', mode='w+', header=False)
 
     #def dict_of_stocks_bought(self, dataframe):
 
 class Marketeer():
     def main():
-        stocker = Stocker()
         file_handler = FileHandler()
-        _local_demographics_ = stocker.get_stock_demographics(file_handler.read_user_file('stock_err.csv'))
+        data = file_handler.read_user_file('stock_err.csv')
+        stocker = Stocker(data)
+        _local_demographics_ = stocker.get_stock_demographics(data)
+        file_handler.write_user_file(_local_demographics_)
         print(_local_demographics_)
         
     if __name__ == "__main__":
