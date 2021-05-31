2d70444c677403d24f1875543f1f1a9b30b45c2a
ravi <rkant062@gmail.com>
2021-05-31 14:17:05 +0530

    Change

--- GetemAll/HttpExample.cs
+++ GetemAll/HttpExample.cs
@@ -17,7 +17,7 @@ namespace GetemAll
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("YAY HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
@@ -26,7 +26,7 @@ namespace GetemAll
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "This HTwohooooecuted successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
